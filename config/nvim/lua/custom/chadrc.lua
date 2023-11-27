41b240fcd47ed8ad9376bfd709d49368552bd0b0
AXWTV <azmainsharer@gmail.com>
2023-11-27 14:55:12 +0600

    NeoVim Config Add

---
+++ config/nvim/lua/custom/chadrc.lua
@@ -0,0 +1,4 @@
+---@type ChadrcConfig 
+ local M = {}
+ M.ui = {theme = 'tokyonight'}
+ return M
--- release.sh
+++ release.sh
@@ -13,32 +13,32 @@ RESET=$(tput sgr0)
 printf "${NOTE} Downloading / Checking for existing Hyprland-Dots.tar.gz...\n"
 
 # Check if Hyprland-Dots.tar.gz exists
-if [ -f Hyprland-Dots.tar.gz ]; then
-  printf "${NOTE} Hyprland-Dots.tar.gz found.\n"
+if [ -f Hyprland-DotFiles.tar.gz ]; then
+  printf "${NOTE} Hyprland-DotFiles.tar.gz found.\n"
 
   # Get the version from the existing tarball filename
   existing_version=$(echo Hyprland-Dots.tar.gz | grep -oP 'v\d+\.\d+\.\d+' | sed 's/v//')
 
   # Fetch the tag_name for the latest release using the GitHub API
-  latest_version=$(curl -s https://api.github.com/repos/AXWTV/Hyprland-Dots/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
+  latest_version=$(curl -s https://api.github.com/repos/AXWTV/Hyprland-DotFiles/releases/latest | grep "tag_name" | cut -d '"' -f 4 | sed 's/v//')
 
   # Check if versions match
   if [ "$existing_version" = "$latest_version" ]; then
-    echo -e "${OK} Hyprland-Dots.tar.gz is up-to-date with the latest release ($latest_version)."
+    echo -e "${OK} Hyprland-DotFiles.tar.gz is up-to-date with the latest release ($latest_version)."
     
     # Sleep for 10 seconds before exiting
     printf "${NOTE} No update found. Sleeping for 10 seconds...\n"
     sleep 10
     exit 0
   else
-    echo -e "${WARN} Hyprland-Dots.tar.gz is outdated (Existing version: $existing_version, Latest version: $latest_version)."
+    echo -e "${WARN} Hyprland-DotFiles.tar.gz is outdated (Existing version: $existing_version, Latest version: $latest_version)."
     read -p "Do you want to upgrade to the latest version? (y/n): " upgrade_choice
     if [ "$upgrade_choice" = "y" ]; then
 		echo -e "${NOTE} Proceeding to download the latest release."
 		
 		# Delete existing directories starting with AXWTV-Hyprland-Dots
-      find . -type d -name 'AXWTV-Hyprland-Dots*' -exec rm -rf {} +
-      rm -f Hyprland-Dots.tar.gz
+      find . -type d -name 'AXWTV-Hyprland-DotFiles*' -exec rm -rf {} +
+      rm -f Hyprland-DotFiles.tar.gz
       printf "${WARN} Removed existing Hyprland-Dots.tar.gz.\n"
     else
       echo -e "${NOTE} User chose not to upgrade. Exiting..."
@@ -50,7 +50,7 @@ fi
 printf "${NOTE} Downloading the latest Hyprland source code release...\n"
 
 # Fetch the tag name for the latest release using the GitHub API
-latest_tag=$(curl -s https://api.github.com/repos/AXWTV/Hyprland-Dots/releases/latest | grep "tag_name" | cut -d '"' -f 4)
+latest_tag=$(curl -s https://api.github.com/repos/AXWTV/Hyprland-DotFiles/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 
 # Check if the tag is obtained successfully
 if [ -z "$latest_tag" ]; then
@@ -59,7 +59,7 @@ if [ -z "$latest_tag" ]; then
 fi
 
 # Fetch the tarball URL for the latest release using the GitHub API
-latest_tarball_url=$(curl -s https://api.github.com/repos/AXWTV/Hyprland-Dots/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
+latest_tarball_url=$(curl -s https://api.github.com/repos/AXWTV/Hyprland-DotFiles/releases/latest | grep "tarball_url" | cut -d '"' -f 4)
 
 # Check if the URL is obtained successfully
 if [ -z "$latest_tarball_url" ]; then
@@ -68,7 +68,7 @@ if [ -z "$latest_tarball_url" ]; then
 fi
 
 # Get the filename from the URL and include the tag name in the file name
-file_name="Hyprland-Dots-${latest_tag}.tar.gz"
+file_name="Hyprland-DotFiles-${latest_tag}.tar.gz"
 
 # Download the latest release source code tarball to the current directory
 if curl -L "$latest_tarball_url" -o "$file_name"; then
@@ -76,7 +76,7 @@ if curl -L "$latest_tarball_url" -o "$file_name"; then
   tar -xzf "$file_name" || exit 1
 
   # delete existing Hyprland-Dots
-  rm -rf AXWTV-Hyprland-Dots
+  rm -rf AXWTV-Hyprland-DotFiles
 
   # Identify the extracted directory
   extracted_directory=$(tar -tf "$file_name" | grep -o '^[^/]\+' | uniq)
@@ -84,7 +84,7 @@ if curl -L "$latest_tarball_url" -o "$file_name"; then
   # Rename the extracted directory to AXWTV-Hyprland-Dots
   mv "$extracted_directory" AXWTV-Hyprland-Dots || exit 1
 
-  cd "AXWTV-Hyprland-Dots" || exit 1
+  cd "AXWTV-Hyprland-DotFiles" || exit 1
 
   # Set execute permission for copy.sh and execute it
   chmod +x copy.sh
